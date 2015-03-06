0ed35cc08717d837aa418e56ad7b257687498784
010-2333-0002 <yunige@yunige-ui-MacBook-Air.local>
2015-03-07 04:10:17 +0900

    어바웃 height 값 단위를 %에서 px으로 변경

--- js/common.js
+++ js/common.js
@@ -137,3 +137,14 @@ if(W > 959){
 }
 
 });
+
+
+$('a[href^=#]').on("click",function(){
+    var t= $(this.hash);
+    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
+    if(t.length){
+        var tOffset=t.offset().top;
+        $('html,body').animate({scrollTop:tOffset-20},'slow');
+        return false;
+    }
+});
